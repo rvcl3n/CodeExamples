e36db61c98d9ea7a102490c4ec75c90cf0f9c74c
rvcl3n <d.rudenko.1996@gmail.com>
2020-08-26 22:17:26 +0300

    Initial commit Solution and gitignore

---
+++ CodeExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
