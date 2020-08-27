64fa0683fcc1ea54176133c60d9b189e12521c15
rvcl3n <d.rudenko.1996@gmail.com>
2020-08-27 21:07:43 +0300

    Added Adapter Example

---
+++ CodeExamples/AdapterExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeExamples
+{
+    public interface ITarget
+    {
+        string GetRequest();
+    }
+
+    // The Adaptee contains some useful behavior, but its interface is
+    // incompatible with the existing client code. The Adaptee needs some
+    // adaptation before the client code can use it.
+    class Adaptee
+    {
+        public string GetSpecificRequest()
+        {
+            return "Specific request.";
+        }
+    }
+
+    // The Adapter makes the Adaptee's interface compatible with the Target's
+    // interface.
+    class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string GetRequest()
+        {
+            return $"This is '{this._adaptee.GetSpecificRequest()}'";
+        }
+    }
+}
