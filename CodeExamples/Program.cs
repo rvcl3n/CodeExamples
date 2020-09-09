725dfcf456de21731c648510077108433d2141c5
rvcl3n <d.rudenko.1996@gmail.com>
2020-09-09 17:17:52 +0300

    Added a Mediator example

--- CodeExamples/Program.cs
+++ CodeExamples/Program.cs
@@ -70,6 +70,19 @@ namespace CodeExamples
             abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
             clientForBridge.ClientCode(abstraction);
 
+            //Mediator call
+            Component1 component1 = new Component1();
+            Component2 component2 = new Component2();
+            new ConcreteMediator(component1, component2);
+
+            Console.WriteLine("Client triggets operation A.");
+            component1.DoA();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client triggers operation D.");
+            component2.DoD();
+
             //Array Sum up
             int[] array = {1,2,3,4,5,6,7,8,9,10};
             Console.WriteLine(ArrayOperations.SumUpArray(array));
