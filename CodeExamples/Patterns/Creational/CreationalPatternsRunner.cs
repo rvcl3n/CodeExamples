fd86e13361ad77744932a5f9d2a17fb9e5b84309
rvcl3n <d.rudenko.1996@gmail.com>
2020-09-18 17:11:59 +0300

    Added an Abstract Factory pattern

--- CodeExamples/Patterns/Creational/CreationalPatternsRunner.cs
+++ CodeExamples/Patterns/Creational/CreationalPatternsRunner.cs
@@ -8,8 +8,14 @@ namespace CodeExamples.Patterns.Creational
     {
         public static void Run()
         {
-            //FabricMethod call
-            new Client().Main();
+            //FactoryMethod call
+            Console.WriteLine("App: Launched with the ConcreteCreator1.");
+            ClientFactoryMethod.ClientCode(new ConcreteCreator1());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("App: Launched with the ConcreteCreator2.");
+            ClientFactoryMethod.ClientCode(new ConcreteCreator2());
 
             //Singleton call
             Singleton singleton1 = Singleton.Instance;
@@ -23,6 +29,15 @@ namespace CodeExamples.Patterns.Creational
             {
                 Console.WriteLine("Singleton failed, variables contain different instances.");
             }
+
+            //AbstractFactory call
+            // The client code can work with any concrete factory class.
+            Console.WriteLine("Client: Testing client code with the first factory type...");
+            ClientAbstractFactory.ClientMethod(new ConcreteFactory1());
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Testing the same client code with the second factory type...");
+            ClientAbstractFactory.ClientMethod(new ConcreteFactory2());
         }
     }
 }
--- CodeExamples/Patterns/Creational/FactoryMethodExample.cs
+++ CodeExamples/Patterns/Creational/FactoryMethodExample.cs
@@ -78,24 +78,13 @@ namespace CodeExamples.Patterns.Creational
         }
     }
 
-    class Client
+    class ClientFactoryMethod
     {
-        public void Main()
-        {
-            Console.WriteLine("App: Launched with the ConcreteCreator1.");
-            ClientCode(new ConcreteCreator1());
-
-            Console.WriteLine("");
-
-            Console.WriteLine("App: Launched with the ConcreteCreator2.");
-            ClientCode(new ConcreteCreator2());
-        }
-
         // The client code works with an instance of a concrete creator, albeit
         // through its base interface. As long as the client keeps working with
         // the creator via the base interface, you can pass it any creator's
         // subclass.
-        public void ClientCode(Creator creator)
+        public static void ClientCode(Creator creator)
         {
             // ...
             Console.WriteLine("Client: I'm not aware of the creator's class," +
