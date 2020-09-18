2cadc4149c639a31548dbc1bb110bd1c527b37fc
rvcl3n <d.rudenko.1996@gmail.com>
2020-09-18 17:23:14 +0300

    Added a Prototype pattern

--- CodeExamples/Patterns/Creational/CreationalPatternsRunner.cs
+++ CodeExamples/Patterns/Creational/CreationalPatternsRunner.cs
@@ -38,6 +38,41 @@ namespace CodeExamples.Patterns.Creational
 
             Console.WriteLine("Client: Testing the same client code with the second factory type...");
             ClientAbstractFactory.ClientMethod(new ConcreteFactory2());
+
+            //Prototype call
+            Person p1 = new Person();
+            p1.Age = 42;
+            p1.BirthDate = Convert.ToDateTime("1977-01-01");
+            p1.Name = "Jack Daniels";
+            p1.IdInfo = new IdInfo(666);
+
+            // Perform a shallow copy of p1 and assign it to p2.
+            Person p2 = p1.ShallowCopy();
+            // Make a deep copy of p1 and assign it to p3.
+            Person p3 = p1.DeepCopy();
+
+            // Display values of p1, p2 and p3.
+            Console.WriteLine("Original values of p1, p2, p3:");
+            Console.WriteLine("   p1 instance values: ");
+            ClientPrototype.DisplayValues(p1);
+            Console.WriteLine("   p2 instance values:");
+            ClientPrototype.DisplayValues(p2);
+            Console.WriteLine("   p3 instance values:");
+            ClientPrototype.DisplayValues(p3);
+
+            // Change the value of p1 properties and display the values of p1,
+            // p2 and p3.
+            p1.Age = 32;
+            p1.BirthDate = Convert.ToDateTime("1900-01-01");
+            p1.Name = "Frank";
+            p1.IdInfo.IdNumber = 7878;
+            Console.WriteLine("\nValues of p1, p2 and p3 after changes to p1:");
+            Console.WriteLine("   p1 instance values: ");
+            ClientPrototype.DisplayValues(p1);
+            Console.WriteLine("   p2 instance values (reference values have changed):");
+            ClientPrototype.DisplayValues(p2);
+            Console.WriteLine("   p3 instance values (everything was kept the same):");
+            ClientPrototype.DisplayValues(p3);
         }
     }
 }
